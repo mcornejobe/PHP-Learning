39303e2e8ba6bf9715803b86f5f3158a7e9378be
Moises Cornejo <moises.cornejo@superpet.pe>
2019-03-20 17:35:52 -0500

    CHALLENGE-Create a function that validates the years and adding index template

--- index.php
+++ index.php
@@ -16,31 +16,24 @@
 
 <body>
 <div class="container">
-  <h1> Pregunta 1: <br>
-    <?php 
+  <h1> Challenge:<br>
+    <?php
+      $months = 4;
+    
+      function getDuration($months){
+        $years = floor($months/12);
+        $extraMonths = $months % 12; // Residuo
+        
+        if($years < 1){
+          return "$extraMonths months";
+        } else if($extraMonths < 1){
+          return "$years years";
+        } else {
+          return "$years years $extraMonths months";
+        }
+      }
 
-    $resultado1 = 32 + 3;
-    $resultado2 = 3*(2+3);
-
-    echo 'Este es el resultado N°1: '.$resultado1. '<br>';
-    echo 'Este es el resultado N°2: '.$resultado2;
-
-    ?>
-  </h1>
-  <hr>
-  <h1>Pregunta 2: <br>
-    <?php 
-
-      $valor1 = 7;
-      $valor2 = 3;
-      $valor3 = '10';
-      $valor4 = 4;
-
-      if($valor1 >5 && $valor1 <10){echo '$valor es mayor que 5 pero menor que 10 <br>';};
-      if($valor2 >=0 && $valor2 <=20){echo '$valor es mayor o igual a 0 pero menor o igual a 20 <br>';};
-      if($valor3 ==='10'){echo '$valor es igual a “10” asegurando que el tipo de dato sea cadena <br>';};
-      if(($valor4 > 0 && $valor4 < 5) || ($valor4 < 10 && $valor4 >15)){echo '$valor es mayor a 0 pero menor a 5 o es mayor a 10 pero menor a 15';};
-
+      echo getDuration($months);
     ?>
   </h1>
 </div>
